dd57900ecc173cc7c6ba5ca8e335a7f3e7a2ae0d
Thomas LECLERCQ <degnus@gmail.com>
2017-02-28 18:16:09 +0530

    Add config file

--- cli.php
+++ cli.php
@@ -4,43 +4,22 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Inbox.php';
 require __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'EmailSender.php';
 
-$cli = new Commando\Command();
-
-$cli->option('l')
-  ->aka('log-level')
-  ->describedAs('log Level (info, warning, debug)')
-  ->must(function($val) {
-      $levels = ['info', 'warning', 'debug'];
-      return in_array($val, $levels);
-  })
-  ->default('info');
-
-$cli->option('n')
-  ->aka('dry-run')
-  ->describedAs('perform a trial run without sending emails')
-  ->boolean()
-  ->default(false);
-
-$options = [
-  'dry-run' => $cli['dry-run'],
-  'log-level' => $cli['log-level']
-];
-
-var_dump($options);
+$options = parseArguments();
+$config = parse_ini_file('config.ini', true);
 
 $logger = new Katzgrau\KLogger\Logger(__DIR__ . DIRECTORY_SEPARATOR . 'logs', $options['log-level']);
 
 $inbox = new Inbox(
-  '{imap.gmail.com:993/imap/ssl}INBOX',
-  'ttest@milend.com',
-  'thomas1234',
+  $config['inbox']['imap'],
+  $config['inbox']['username'],
+  $config['inbox']['password'],
   $options
 );
 
 $emailSender = new EmailSender(
-  'smtp.gmail.com',
-  'ttest@milend.com',
-  'thomas1234',
+  $config['outgoing']['host'],
+  $config['outgoing']['username'],
+  $config['outgoing']['password'],
   $inbox,
   $options
 );
@@ -60,5 +39,30 @@ if ($inbox->loggedIn() && $emailSender->loggedIn()) {
   echo 'An error happened (check the logs)' . PHP_EOL;
 }
 
+
+function parseArguments() {
+  $cli = new Commando\Command();
+
+  $cli->option('l')
+    ->aka('log-level')
+    ->describedAs('log Level (info, warning, debug)')
+    ->must(function($val) {
+        $levels = ['info', 'warning', 'debug'];
+        return in_array($val, $levels);
+    })
+    ->default('info');
+
+  $cli->option('n')
+    ->aka('dry-run')
+    ->describedAs('perform a trial run without sending emails')
+    ->boolean()
+    ->default(false);
+
+  $options = [
+    'dry-run' => $cli['dry-run'],
+    'log-level' => $cli['log-level']
+  ];
+  return $options;
+}
 
 //print_r($emails);
